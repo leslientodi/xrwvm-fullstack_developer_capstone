124d81a706dd21596c0d9ff500261fca3d10e4bb
leslientodi <lesliehazard500@gmail.com>
2025-01-16 10:05:59 -0500

    about us page

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','xrwvm-fullstack_developer_capstone']
-CSRF_TRUSTED_ORIGINS = ['xrwvm-fullstack_developer_capstone']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
